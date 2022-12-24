48c3ae44cbbc1f7626bd66f74861c7b78c2316d8
杨再润 <yangzair@outlook.com>
2022-12-24 14:43:45 +0800

    [实现]向HBase写数据

--- HBaseCon.py
+++ HBaseCon.py
@@ -19,43 +19,25 @@ tables = connection.tables()
 print(tables)
 #查看表'ETC'的所有数据，toStr=True表示将数据转换为字符串
 
-table = connection.table('ETC')
+table = connection.table('static')
 for key, data in table.scan():
     print(key.decode('utf-8'), data)
 #在表’test’中插入一条数据,数据格式为：b'i这个度': {b'info:col1': b'value1'}，中文编码为ascii
 
 # table.put('i这个度'.encode(), {b'info:col1': b'value1'})
 
-#将data/data1.txt中的数据插入到表'test'中
-# with open('data/data1.txt', 'r') as f:
-#     for line in f:
-#         line = line.strip()
-#         key, value = line.split(',')
-#         table.put(key.encode(), {b'info:col1': value.encode()})
+bat = table.batch()
 
-#在创建表'static',列族有'LTZX','SLD','SSHN'
-# connection.create_table('static', {'LTZX': dict(), 'SLD': dict(), 'SSHN': dict()})
-'''
-table = connection.table('static')
+#将data/data1.txt中的数据插入到表'test'中
 with open('data/data1.csv', 'r') as f:
     for line in f:
         line = line.strip()
         if line[0] == 'D':
             continue
-        key, value = line.split(',')[0],line.split(',')[1]
-        print(key,value)
-        table.put(key, {b'LTZX': value.encode()})
+        key, value1,value2,value3 =  line.split(',')
+        bat.put(key, {b'LTZX:minute': value1,b'SLD:minute': value2,b'SSHN:minute': value3})
+bat.send()
 
-for key, data in table.scan():
-    print(key.decode('utf-8'), data)
+#在创建表'static',列族有'LTZX','SLD','SSHN'
+# connection.create_table('static', {'LTZX': dict(), 'SLD': dict(), 'SSHN': dict()})
 
-pool = happybase.ConnectionPool(size=3,host="node2",port=9090,protocol='compact',transport='framed')
-# 从连接池中取出一个连接
-with pool.connection() as conn:
-    table = conn.table('test')
-    # 查看表中的数据
-    for key, data in table.scan():
-        print(key.decode('utf-8'), data)
-    # 操作完成 及时close 保证每次操作都是一个新启动的连接
-    conn.close()
-'''
